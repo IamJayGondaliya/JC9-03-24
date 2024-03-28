1d98bc363feacf97013535b6b24050b8de328c03
IamJayGondaliya <prof.rnwjay2205@gmail.com>
2024-03-28 11:06:29 +0530

    New changes...

---
+++ CH-2/1.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a = 10, b = 20;
+	char ch = 'Ja';
+	float pi = 3.14;
+	clrscr();
+	// Datatype variableName;
+
+	printf("A\t: %d\n",a);//10
+	printf("B\t: %d\n\n",b);//20
+
+	printf("Ch\t: %c\n\n",ch);
+
+	printf("PI\t: %.2f",pi);// 3.140000
+
+
+	getch();
+}
---
+++ CH-2/2.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a = 10, b = 20;
+	clrscr();
+
+
+	printf("Value  of %d and %d ans = %d\n",a,b,a+b);
+	printf("%d * %d = %d\n",a,b,b/a);
+
+	getch();
+}
