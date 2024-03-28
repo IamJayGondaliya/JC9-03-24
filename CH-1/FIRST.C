badd593352a382320e5370d52efd736ad2d340ce
IamJayGondaliya <prof.rnwjay2205@gmail.com>
2024-03-28 10:59:34 +0530

    Ch-1: Intro to C language

---
+++ CH-1/FIRST.C
@@ -0,0 +1,17 @@
+#include <stdio.h> // Stander Input Output Header File
+#include <conio.h> // Console Input Output Header File
+// # => Pre Processer
+
+
+
+
+// This is Body for C Programming Language
+main()
+{
+	clrscr(); // To clear screen for perview output
+	// clrscr stand for C\lear Screen
+
+	printf("Hello C Language");
+
+	getch(); // To Hold Console(Output) Screen
+}
---
+++ CH-1/SECOND.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	clrscr();
+
+	printf("Hello\tWorld !!\n");
+	printf("Progra\bmming\tLanguage\n\n");
+
+	printf("Name\t: Jayn\b
+	n");
+	printf("Age\t: 10\n");
+	printf("School\t: Red & White..");
+
+	getch();
+}
